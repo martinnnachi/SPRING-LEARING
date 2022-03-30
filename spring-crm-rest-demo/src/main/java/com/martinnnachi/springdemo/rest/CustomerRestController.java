2f1c64cc1fa11e97f7fa19db8f593e88e082d951
Martin Nnachi <mnnachi@gmail.com>
2022-03-30 19:06:25 +0100

    added exception handling to project and added new customer

--- spring-crm-rest-demo/src/main/java/com/martinnnachi/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/martinnnachi/springdemo/rest/CustomerRestController.java
@@ -2,10 +2,7 @@ package com.martinnnachi.springdemo.rest;
 
 import com.martinnnachi.springdemo.entity.Customer;
 import com.martinnnachi.springdemo.service.CustomerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,7 +33,27 @@ public class CustomerRestController {
 
     @GetMapping("/customers/{customerId}")
     public Customer getCustomer(@PathVariable int customerId) {
-        return customerService.getCustomer(customerId);
+        Customer theCustomer = customerService.getCustomer( customerId );
+
+        if (theCustomer == null) {
+            throw new CustomerNotFoundException( "Customer id not found: " + customerId );
+        }
+
+        return theCustomer;
+    }
+
+    // add mapping for POST /customers - add a new customer
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer theCustomer){
+
+        // also, just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of a new item ... instead of update
+        theCustomer.setId( 0 );
+
+        customerService.saveCustomer( theCustomer );
+
+
+        return theCustomer;
     }
 
 
