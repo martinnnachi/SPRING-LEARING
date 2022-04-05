ce2bd0959325c96c22194823ab6dceb3469717a3
martinnnachi <mnnachi@gmail.com>
2022-04-05 19:28:13 +0100

    Spring Data REST

---
+++ 23-spring-data-rest-cruddemo/src/main/java/com/martinnnachi/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.martinnnachi.cruddemo.dao;
+
+import com.martinnnachi.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    
+}
