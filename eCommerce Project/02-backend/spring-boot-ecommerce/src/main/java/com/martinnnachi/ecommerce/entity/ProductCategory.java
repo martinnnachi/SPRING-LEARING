f5346d3a7e97058e692ba140b09295387bd67ed5
martinnnachi <mnnachi@gmail.com>
2022-04-08 19:55:15 +0100

    project initial set up and testing

---
+++ eCommerce Project/02-backend/spring-boot-ecommerce/src/main/java/com/martinnnachi/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,25 @@
+package com.martinnnachi.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product_category")
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private  long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
